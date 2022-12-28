4ab8f1bf7936740e000977121f414336df997bdd
Ross-Berry <93287406+Ross-Berry@users.noreply.github.com>
2022-12-28 16:46:35 +0000

    If applied, this commit will complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let min = num1;
+    let max = num2;
+    
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    } 
+    
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
 
+    if (num1 > num2) {
+        min = num2;
+        max = num1;
+    }
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
