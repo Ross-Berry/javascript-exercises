27c6d4ff63e31a18a7c1f3eaf9958c196e096ec0
Ross-Berry <93287406+Ross-Berry@users.noreply.github.com>
2023-01-27 15:35:49 +0000

    If applied, this commit will complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let x = 1;
+  let y = 1;
+  let fib;
 
+  if (n <= 0) {
+    return "OOPS";
+  }
+
+  if (n <= 2) {
+    return 1;
+  }
+
+  for (let i = 0; i < n - 2; i++) {
+    fib = x + y;
+    x = y;
+    y = fib;
+  }
+
+  return fib;
 };
 
 // Do not edit below this line
