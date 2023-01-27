1580cfa2d23034960b9541e3f604a8407d53f271
Ross-Berry <93287406+Ross-Berry@users.noreply.github.com>
2023-01-27 16:30:04 +0000

    If applied, this commit will complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const yearAsOfToday = new Date();
+  let oldestAge = 0;
+  let oldestPerson = {};
 
+  people.map((person) => {
+    let age =
+      (person.yearOfDeath || yearAsOfToday.getFullYear()) - person.yearOfBirth;
+
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldestPerson = person;
+    }
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
