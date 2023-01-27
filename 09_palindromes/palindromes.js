58ea338c4953827c20baee1938d861043c1ef461
Ross-Berry <93287406+Ross-Berry@users.noreply.github.com>
2023-01-27 15:56:40 +0000

    If applied, this commit will complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const punc = new RegExp(/[!\s,.']/g);
+  const newStr = str.replace(punc, "").toLowerCase();
+  const palindrome = newStr.split("").reverse().join("");
 
+  if (palindrome === newStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
